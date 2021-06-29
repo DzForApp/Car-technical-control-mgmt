47be38b24f7116a1b9244dad187426bc7e6fe8dd
Mr_R <dzforapp@gmail.com>
2021-06-29 02:21:39 +0200

    car_control app 2nd commit

--- Settings/asgi.py
+++ Settings/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mycar project.
+ASGI config for Settings project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Settings.settings')
 
 application = get_asgi_application()
--- car_control/views.py
+++ car_control/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import car
+from django.urls import reverse
+from django.views import generic
 
+class IndexView(generic.ListView):
+    template_name = 'car_control/index.html'
+    context_object_name = 'latest_car_list'
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return car.objects.order_by('-mat')[:5]
+
+class DetailView(generic.DetailView):
+    model = car
+    template_name = 'car_control/detail.html'
